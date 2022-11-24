b3319b2b8410af219ed04f3ceb4004df9caa050e
kacperkwa <kacperkwartaxz@gmail.com>
2022-11-24 19:27:24 +0100

    added mobile navigation, and open/close script on button click

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const menuButton = document.querySelector('.nav-container__menu-button');
+const navigationMobile = document.querySelector('.navigation-mobile');
+const menuButtonImg = document.querySelector('.menu-button-img');
+
+const showMenu = () => {
+	navigationMobile.classList.toggle('active');
+	menuButtonImg.classList.toggle('menu-button-img-change');
+};
+
+menuButton.addEventListener('click', showMenu);
